9e85af9770ecb878be394c57a37063b54f5fa5e3
Avinash Kumar <avinash.kumar@msci.com>
2022-06-15 19:18:00 +0530

    added resource sample

---
+++ examples/sample-resource.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    inviulrestapi = {
+      version = "0.1.6"
+      source  = "inviul/inviulrestapi"
+    }
+  }
+}
+
+provider "inviulrestapi" {}
+
+resource "inviulrestapi_resource" "myFirstRestCall_r" {
+  base_uri = "https://username:password@dev.test.com?"
+  path = "trailpath"
+  http_rest_method = "PATCH"
+  json_payload = "[{\"key\":\"value\"}]"
+}
+
+
+
+# Returns Output
+output "myRestCallOutput" {
+  value = inviulrestapi_resourcee.myFirstRestCall_r.rest_out
+}
+
